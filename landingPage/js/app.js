c13f9c3a234fbbba013027957bd1eba423797f2c
Kasia Wrzesniak <wrzesniak.k@gmail.com>
2022-11-08 20:41:28 +0100

    all bullshit back

--- landingPage/js/app.js
+++ landingPage/js/app.js
@@ -1,3 +1,10 @@
+/* Template Name: Neloz - Responsive Bootstrap 4 Landing Page Template
+   Author: Themesdesign
+   Version: 1.0.0
+   Created: Jan 2019
+   File Description: Main js file
+*/
+
 (function ($) {
 
     'use strict';
@@ -48,6 +55,41 @@
 	        }
 	    }
 	})
+
+	// contact
+
+	$('#contact-form').submit(function() {
+
+	    var action = $(this).attr('action');
+
+	    $("#message").slideUp(750, function() {
+	        $('#message').hide();
+
+	        $('#submit')
+	            .before('')
+	            .attr('disabled', 'disabled');
+
+	        $.post(action, {
+	                name: $('#name').val(),
+	                email: $('#email').val(),
+	                comments: $('#comments').val(),
+	            },
+	            function(data) {
+	                document.getElementById('message').innerHTML = data;
+	                $('#message').slideDown('slow');
+	                $('#cform img.contact-loader').fadeOut('slow', function() {
+	                    $(this).remove()
+	                });
+	                $('#submit').removeAttr('disabled');
+	                if (data.match('success') != null) $('#cform').slideUp('slow');
+	            }
+	        );
+
+	    });
+
+	    return false;
+
+	});
 
 	// loader
 	$(window).on('load', function() {
